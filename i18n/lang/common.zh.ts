f4bda49d65f1fe793157ca7034a01a9f25520875
xingyunfei <keiumhii@gmail.com>
2023-11-18 00:02:15 +0800

    feat: 中文转繁体

--- i18n/lang/common.zh.ts
+++ i18n/lang/common.zh.ts
@@ -1,22 +1,21 @@
 const translation = {
   api: {
     success: '成功',
-    saved: '已保存',
-    create: '已创建',
+    saved: '已儲存',
+    create: '已建立',
   },
   operation: {
-    confirm: '确认',
+    confirm: '確認',
     cancel: '取消',
     clear: '清空',
-    save: '保存',
-    edit: '编辑',
-    refresh: '重新开始',
-    search: '搜索',
-    send: '发送',
-    lineBreak: '换行',
-    like: '赞同',
-    dislike: '反对',
+    save: '儲存',
+    edit: '編輯',
+    refresh: '重新開始',
+    search: '搜尋',
+    發送: '發送',
+    lineBreak: '換行',
+    like: '贊同',
+    dislike: '反對',
   },
 }
-
 export default translation
